7653ae838643034e938c2dd88c2f6539ab128412
Joksa <nikolajoksimovic419@gmail.com>
2023-04-01 11:54:44 +0200

    added some validations

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -44,36 +44,44 @@ exports.register = [
     .trim()
     .isLength({ min: 1 })
     .escape(),
-  /*body("birthday")
-    .optional({ checkFalsy: true })
-    .isISO8601()
-    .toDate(),*/
 
   async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(400).json(errors.array());
 
-    const user = new User({
-      username: req.body.username,
-      password: await bcrypt.hash(req.body.password, 10),
-      firstName: req.body.firstName,
-      lastName: req.body.lastName
-    }).save().then(() => {
+    try {
+      const existingUser = await User.findOne({ username: req.body.username });
+      if (existingUser) {
+        throw new Error("Username already exists");
+      }
+
+      const user = new User({
+        username: req.body.username,
+        password: await bcrypt.hash(req.body.password, 10),
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+      })
+      await user.save()
       console.log("User successfully registered")
-    }).catch(err => {
-      return next(err)
-    })
+    } catch (e) {
+      console.log(e)
+      res.status(401).json({ message: e.message });
+    }
   }
 ]
 
 exports.login = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.body.username }).select("+password");
-    const isValidPassword = await bcrypt.compare(req.body.password, user.password)
+    if (!user) {
+      throw new Error("User not found");
+    }
 
+    const isValidPassword = await bcrypt.compare(req.body.password, user.password)
     if (!isValidPassword) {
       throw new Error("Incorrect password")
     }
+
     console.log("Correct password")
     const token = jwt.sign({
       userId: user._id,
@@ -93,7 +101,7 @@ exports.login = async (req, res, next) => {
     })
   } catch (e) {
     console.log("Error", e)
-    next(e);
+    res.status(401).json({ message: e.message });
   }
 }
 

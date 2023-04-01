6beca1908ebf325e8e1f3a6cc6917b95de89e6fe
Joksa <nikolajoksimovic419@gmail.com>
2023-04-01 13:03:21 +0200

    fixed cors hopefully

--- server.js
+++ server.js
@@ -16,16 +16,10 @@ async function main() {
   console.log("Connected to database")
 }
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(cors({
   origin: 'https://odinbook-frontend.netlify.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  credentials: true,
+  optionSuccessStatus: 200
 }));
 
 app.use(express.json());
